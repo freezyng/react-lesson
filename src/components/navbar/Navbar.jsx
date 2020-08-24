6846cf610a57e5129dd02c2af4ecd04a4d73ce82
Artyom Freezyng <tema9651@yandex.ru>
2020-08-24 19:25:15 +0300

    remade users function component => users class component

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -7,19 +7,19 @@ const Navbar = () => {
         <div className="navbar">
             <div className="navbar__menu">
                 <div className="navbar__link link-item">
-                    <NavLink to='/profile'>профиль</NavLink>
+                    <NavLink to='/profile'>Профиль</NavLink>
                 </div>
                 <div className="navbar__link link-item">
-                    <NavLink to='/dialogs'>диалоги</NavLink>
+                    <NavLink to='/dialogs'>Диалоги</NavLink>
                 </div>
                 <div className="navbar__link link-item">
-                    <NavLink to='/music'>музыка</NavLink>
+                    <NavLink to='/music'>Музыка</NavLink>
                 </div>
                 <div className="navbar__link link-item">
-                    <NavLink to='/news'>новости</NavLink>
+                    <NavLink to='/news'>Новости</NavLink>
                 </div>
                 <div className="navbar__link link-item">
-                    <NavLink to='/users'>поиск пользователя</NavLink>
+                    <NavLink to='/users'>Поиск пользователей</NavLink>
                 </div>
             </div>
         </div>
--- src/components/users/Users.jsx
+++ src/components/users/Users.jsx
@@ -1,47 +1,48 @@
 import React from 'react';
 import './users.css';
 import * as axios from 'axios';
-import userAvatar from '../../assets/images/User.png'
+import userAvatar from '../../assets/images/User.png';
 
-let Users = (props) => {
+class Users extends React.Component {
 
-    
+    constructor(props) { 
+        super(props);
 
-    if(!props.users.length) {
         axios.get('https://social-network.samuraijs.com/api/1.0/users').then( (response) => {
-        props.setUsers([ ...response.data.items ])
-    })
+            this.props.setUsers([ ...response.data.items ])
+        });
+        
     }
 
-
-
-
-    return <div className='users'>
-        {
-            props.users.map( (u) => {
-                return (
-                <div key={u.id} className='user'>
-                    <div>
-                        <div className='user__avatar'>
-                            <img src={u.photos.large !== null ? u.photos.large : userAvatar } />
+    render() {
+        return <div className='users'>
+            {
+                this.props.users.map( (u) => 
+                {
+                    return (
+                    <div key={u.id} className='user'>
+                        <div>
+                            <div className='user__avatar'>
+                                <img src={u.photos.large !== null ? u.photos.large : userAvatar } />
+                            </div>
+                            <div className='user__btn'>
+                                {u.followed
+                                    ? <button onClick={() => this.props.unfollow(u.id) }>Отписаться</button>
+                                    : <button onClick={() =>  this.props.follow(u.id) }>Подписаться</button>
+                                }
+                            </div>
                         </div>
-                        <div className='user__btn'>
-                            {u.followed
-                                ? <button onClick={() => props.unfollow(u.id) }>Отписаться</button>
-                                : <button onClick={() =>  props.follow(u.id) }>Подписаться</button>
-                            }
+                        <div className='user__info'>
+                            <div className='user__info-name'> {u.name} </div>
+                            <div className='user__info-status'> {u.status} </div>
+                            {/* <div className='user__info-location'> {u.location.city} <br/> {u.location.country} </div> */}
                         </div>
                     </div>
-                    <div className='user__info'>
-                        <div className='user__info-name'> {u.name} </div>
-                        <div className='user__info-status'> {u.status} </div>
-                        {/* <div className='user__info-location'> {u.location.city} <br/> {u.location.country} </div> */}
-                    </div>
-                </div>
-                )
-            })
-        }
-    </div>
+                    )
+                })
+            }
+        </div>
+    }
 }
 
 export default Users;
