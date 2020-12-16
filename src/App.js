59e2136eba69e2bbf397599d271e07f7fd303ebf
Artyom Freezyng <tema9651@yandex.ru>
2020-12-16 14:55:55 +0300

    add lazy, Suspense

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import './App.css';
 import HeaderContainer from './components/header/HeaderContainer';
 import Navbar from './components/navbar/Navbar';
-import ProfileContainer from './components/profile/ProfileContainer';
-import DialogsContainer from './components/dialogs/DialogsContainer';
 import { Route } from 'react-router-dom';
 import UsersContainer from './components/users/UsersContainer';
 import Login from './components/login/Login';
 import { connect } from 'react-redux';
 import { initializeApp } from './redux/app-reducer';
 import preLoader from './assets/images/usersPreloader.svg';
+
+
+const DialogsContainer = React.lazy(() => import('./components/dialogs/DialogsContainer'));
+const ProfileContainer = React.lazy(() => import('./components/profile/ProfileContainer'));
+
 
 class App extends React.Component {
 
@@ -24,34 +27,38 @@ class App extends React.Component {
         }
 
         return (
-                <div className="App">
-                    <HeaderContainer />
-    
-                    <div className="wrap-navbar-content container">
-                        <Navbar />
-    
-                        <Route path='/dialogs' 
-                            render={() => {
-                                return (<DialogsContainer />)
-                            }
-                        }/>
-                        <Route path='/profile/:userId?' 
-                            render={() => {
-                                return (<ProfileContainer />)
-                            }
-                        }/>
-                        <Route path='/users' 
-                            render={() => {
-                                return (<UsersContainer />)
-                            }
-                        }/>
-                        <Route path='/login' 
-                            render={() => {
-                                return (<Login />)
-                            }
-                        }/>
-                    </div>
+            <div className="App">
+                <HeaderContainer />
+
+                <div className="wrap-content container">
+                    <Navbar />
+
+                    <Route path='/dialogs' 
+                        render={() => {
+                            return (<Suspense fallback={<img className='content-loading' src={preLoader} alt='1'/>} >
+                                <DialogsContainer />
+                            </Suspense> )
+                        }
+                    }/>
+                    <Route path='/profile/:userId?' 
+                        render={() => {
+                            return (<Suspense fallback={<img className='content-loading' src={preLoader} alt='1'/>} >
+                                <ProfileContainer />
+                            </Suspense> )
+                        }
+                    }/>
+                    <Route path='/users' 
+                        render={() => {
+                            return (<UsersContainer />)
+                        }
+                    }/>
+                    <Route path='/login' 
+                        render={() => {
+                            return (<Login />)
+                        }
+                    }/>
                 </div>
+            </div>
         );
     }
 }
